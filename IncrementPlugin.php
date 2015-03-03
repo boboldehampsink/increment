da734aef2f778568e446e6bff4b0e948db29a96d
Niek Schmoller <n.schmoller@itmundi.nl>
2015-03-03 14:18:34 +0100

    Prevent duplicate values when multiple people create entries at the same time.

--- IncrementPlugin.php
+++ IncrementPlugin.php
@@ -23,4 +23,30 @@ class IncrementPlugin extends BasePlugin
     {
         return 'http://www.itmundi.nl';
     }
+
+    public function init()
+    {
+        // check increment fields to make sure no duplicate values are inserted
+        craft()->on('entries.beforeSaveEntry', function(Event $event) use ($outer) {
+            $isNewEntry = $event->params['isNewEntry'];
+            $entry = $event->params['entry'];
+
+            // exisiting entries don't change, so no need to check
+            if ($isNewEntry) {
+                $fields = $entry->getFieldLayout()->getFields();
+                foreach ($fields as $field) {
+                    if ($field->getField()->type == 'Increment') {
+                        $handle = $field->getField()->handle;
+                        $max = craft()->db->createCommand()->select('MAX(`field_' . $handle . '`)')->from('content')->queryScalar();
+                        $currentValue = $entry[$handle];
+
+                        // current value should by higher than max, otherwise a duplicate entry could be created
+                        if ($currentValue <= $max) {
+                            $entry->setContentFromPost(array($handle => $max + 1));
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
